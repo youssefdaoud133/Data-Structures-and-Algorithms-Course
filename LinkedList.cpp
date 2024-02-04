16da1951693f22c2215a576baec3b4a0dbec0c85
YoussefDaoud <youssefdaoud0122@gmail.com>
2024-02-04 23:17:18 +0200

    intro to linked list

---
+++ LinkedList.cpp
@@ -0,0 +1,4 @@
+// difference between array and linked list
+// array all element in series in memory e1 -> e2 -> e3
+// linked list  all element not in series in memory e1 ------> e2 ---> e3 -> e4  <---e5
+// in linked list its easy to insert first element 
