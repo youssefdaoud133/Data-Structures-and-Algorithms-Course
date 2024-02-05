66cfef88e8da005e35668f006148ae03dae1bf9a
YoussefDaoud <youssefdaoud0122@gmail.com>
2024-02-05 02:50:07 +0200

    Linked list code and test

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -3,11 +3,11 @@
 // linked list  all element not in series in memory e1 ------> e2 ---> e3 -> e4  <---e5
 // in linked list its easy to insert first element 
 
-
 #include <algorithm>
-
 #include <iostream>
 using namespace std;
+
+
 
 class Node {
 private:
@@ -32,9 +32,133 @@ public:
 	}
 
 	// 
+	friend class LinkedList;
 
 
+};
 
+
+class LinkedList {
+private:
+	Node* Head;
+	Node* tail;
+	
+public:
+	LinkedList(){
+		this->Head = nullptr;		 
+	}
+	bool embty() {
+		if (this->Head == nullptr) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	void push(int d) {
+		Node* n = new Node(d);
+		n->SetNext(this->Head);
+		this->Head = n;
+		
+
+	}
+
+	int length() {
+		Node* CurNode = Head;
+		int length = 0;
+		while (CurNode != nullptr)
+		{
+			length++;
+			CurNode = CurNode->GetNext();
+		}
+
+		return length;
+
+	}
+	void PopFront() {
+		Node* temp = this->Head;
+		if (this->embty()) {
+			return;
+		}
+		else
+		{
+
+		this->Head = (this->Head->GetNext());
+		delete temp;
+		}
+
+	}
+
+
+	
+	void PushBack(int d) {
+		Node* n = new Node(d);
+		if (this->Head == nullptr) {
+			this->Head = n;
+			
+		}
+		else
+		{
+			Node* CurNode = Head;
+			
+			while (CurNode->GetNext() != nullptr)
+			{
+				CurNode = CurNode->GetNext();
+			}
+			CurNode->SetNext(n);
+
+
+			
+			
+
+		}
+		
+	}
+	void PopBack() {
+		if (this->embty()) {
+			return;
+		}
+		if (this->Head->GetNext() == nullptr) {
+			delete Head;
+			this->Head = nullptr;
+			return;
+		}
+
+		Node* LastNode = Head;
+		Node* PreLastNode = nullptr;
+		while (LastNode ->GetNext() != nullptr)
+		{
+			PreLastNode=(LastNode);
+			LastNode = LastNode->GetNext();
+		}
+		delete LastNode;
+		PreLastNode->SetNext(nullptr);
+
+	}
+	void Clear(){
+		while (! this->embty()) {
+			this->PopFront();
+		}
+	}
+	int GetIndex(int index) {
+		if (index < 0 || index >= length()) {
+			cout << "Index out of bounds." << endl;
+			return -1;  // or throw an exception, depending on your design choice
+		}
+
+		Node* FirstElement = Head;
+		for (int i = 0; i < index; i++) {
+			FirstElement = FirstElement->GetNext();
+		}
+
+		return FirstElement->GetData();
+	}
+
+	~LinkedList()
+	{
+		this->Clear();	
+	}
 };
 
 
@@ -42,12 +166,23 @@ public:
 int main()
 
 {
-	Node index1(6);
-	Node index2(70);
-	index1.SetNext(&index2);
-	Node index3(80);
-	index2.SetNext(&index3);
-	cout << (index1.GetNext()->GetData());
+	LinkedList * L1 = new LinkedList;
+	cout << L1->length()<<endl;
+	L1->push(5);
+	L1->push(6);
+	L1->push(4);
+	L1->PushBack(7);
+	cout << L1->length()<<endl;
+	cout << L1->GetIndex(3)<<endl;
+	cout << L1->GetIndex(2)<<endl;
+	L1->PopBack();
+	cout << L1->GetIndex(3)<<endl;
+	cout << L1->length()<<endl;
+	cout << L1->embty() << endl;
+	delete L1;
+
+
+
 
 
 	
