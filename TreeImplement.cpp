f77884694797deaae8292bc74388a15fc475e360
YoussefDaoud <youssefdaoud0122@gmail.com>
2024-02-07 17:46:56 +0200

    TreeBinaryEquation

--- TreeImplement.cpp
+++ TreeImplement.cpp
@@ -1,29 +1,76 @@
 
 #include<iostream>
+#include <string>
+#include <cctype>
 using namespace std;
+bool isNumeric(const std::string& str) {
+	for (char c : str) {
+		if (!std::isdigit(c)) {
+			return false;
+		}
+	}
+	return true;
+}
 
 struct node
 {
-  int data;
-  struct node* left;
-  struct node* right;
+	string data;
+	struct node* left;
+	struct node* right;
 };
 
-struct node* newNode(int idata)
+struct node* newNode(string idata)
 {
-  struct node* node = new struct node;
-  node->data = idata;
-  node->left = NULL;
-  node->right = NULL;
-  return node;
+	struct node* node = new struct node;
+	node->data = idata;
+	node->left = NULL;
+	node->right = NULL;
+	return node;
+}
+
+int Process(int Left , int Right, string Operator ) {
+	if(Operator == "+") {
+		return Left + Right;
+	}if(Operator == "-") {
+		return Left - Right;
+	}if(Operator == "*") {
+		return Left * Right;
+	}if(Operator == "/") {
+		return Left / Right;
+	}
+}
+int TreeBinaryEquation(node* SupTree) {
+	int left;
+	int right;
+	
+	if (isNumeric(SupTree->data)) {
+		return stoi(SupTree->data);
+	}
+	left = TreeBinaryEquation(SupTree->left);
+	right = TreeBinaryEquation(SupTree->right);
+	return Process(left,right, SupTree->data);
+
+	
 }
 
 int main()
 {
-  struct node *root = newNode(9);
-  root->left = newNode(8);
-  (*root).right = newNode(3);
-  root->left->left = newNode(823);
-  cout<<root->left->left->data;
-  return 0;
+	
+	struct node* root = newNode("+");
+	root->left = newNode("*");
+	root->left->left = newNode("2");
+	root->left->right = newNode("-");
+	root->left->right->left = newNode("10");
+	root->left->right->right = newNode("7");
+	root->right = newNode("+");
+	root->right->left = newNode("3");
+	root->right->right = newNode("*");
+	root->right->right->left = newNode("2");
+	root->right->right->right = newNode("4");
+	
+	
+
+	cout<<TreeBinaryEquation(root);
+	
+	return 0;
 }
